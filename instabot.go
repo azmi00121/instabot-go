61da4889d94c1e87fb3b498d0ec212f283f4c702
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2016-11-19 00:01:19 +0530

    add basic

---
+++ instabot.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+const (
+	root     = "https://www.instagram.com/"
+	login    = "https://www.instagram.com/accounts/login/ajax/"
+	logout   = "https://www.instagram.com/accounts/logout/"
+	tag      = "https://www.instagram.com/explore/tags/"
+	photo    = "https://www.instagram.com/p/"
+	like     = "https://www.instagram.com/web/likes/%s/like/"
+	unlike   = "https://www.instagram.com/web/likes/%s/unlike/"
+	comment  = "https://www.instagram.com/web/comments/%s/add/"
+	follow   = "https://www.instagram.com/web/friendships/%s/follow/"
+	unfollow = "https://www.instagram.com/web/friendships/%s/unfollow/"
+)
+
+type Config struct {
+	Username     string   `yaml:"USERNAME"`
+	Password     string   `yaml:"PASSWORD"`
+	Tags         []string `yaml:"TAGS"`
+	TotalLikes   int64    `yaml:"TOTAL_LIKES"`
+	LikesPerUser int64    `yaml:"LIKES_PER_USER"`
+}
+
+func GetConfig() (error, Config) {
+	configFile, err := ioutil.ReadFile("config.yml")
+	if err != nil {
+		log.Fatal("Couldn't find the config file")
+	}
+	config := Config{}
+	err = yaml.Unmarshal(configFile, &config)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	return nil, config
+}
+
+func main() {
+	fmt.Println("Instabot v0.0.1")
+	fmt.Printf("%s", root)
+	_, config := GetConfig()
+	fmt.Printf("\n%v\n", config)
+}
