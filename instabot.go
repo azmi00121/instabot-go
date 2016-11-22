4c2d7b0020628cd0e6c89f71b9057a5b17131e3f
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2016-11-22 12:26:08 +0530

    add cookie based client login support

--- instabot.go
+++ instabot.go
@@ -1,24 +1,31 @@
-package instabot
+package main
 
 import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+
+	"golang.org/x/net/publicsuffix"
 
 	yaml "gopkg.in/yaml.v2"
 )
 
 const (
-	root     = "https://www.instagram.com/"
-	login    = "https://www.instagram.com/accounts/login/ajax/"
-	logout   = "https://www.instagram.com/accounts/logout/"
-	tag      = "https://www.instagram.com/explore/tags/"
-	photo    = "https://www.instagram.com/p/"
-	like     = "https://www.instagram.com/web/likes/%s/like/"
-	unlike   = "https://www.instagram.com/web/likes/%s/unlike/"
-	comment  = "https://www.instagram.com/web/comments/%s/add/"
-	follow   = "https://www.instagram.com/web/friendships/%s/follow/"
-	unfollow = "https://www.instagram.com/web/friendships/%s/unfollow/"
+	root           = "https://www.instagram.com/"
+	login          = "https://www.instagram.com/accounts/login/ajax/"
+	logout         = "https://www.instagram.com/accounts/logout/"
+	tag            = "https://www.instagram.com/explore/tags/"
+	photo          = "https://www.instagram.com/p/"
+	like           = "https://www.instagram.com/web/likes/%s/like/"
+	unlike         = "https://www.instagram.com/web/likes/%s/unlike/"
+	comment        = "https://www.instagram.com/web/comments/%s/add/"
+	follow         = "https://www.instagram.com/web/friendships/%s/follow/"
+	unfollow       = "https://www.instagram.com/web/friendships/%s/unfollow/"
+	acceptLanguage = "en-US,en;q=0.8,pt-br,pt;q=0.6,"
+	userAgent      = "('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 ''(KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36')"
 )
 
 type Config struct {
@@ -47,4 +54,21 @@ func main() {
 	fmt.Printf("%s", root)
 	_, config := GetConfig()
 	fmt.Printf("\n%v\n", config)
+	options := cookiejar.Options{
+		PublicSuffixList: publicsuffix.List,
+	}
+	jar, err := cookiejar.New(&options)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := http.Client{Jar: jar}
+	resp, err := client.PostForm(login, url.Values{
+		"username": {"vinitxxxx"},
+		"password": {"xxlssssinuxxxxx"},
+	})
+	fmt.Println(resp)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
